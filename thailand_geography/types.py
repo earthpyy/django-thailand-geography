8de128896d2d8134966901397bed0d0e946f28d8
Preeti Yuankrathok <preetisatit@gmail.com>
2023-10-12 00:48:33 +0700

    Import geo command

---
+++ thailand_geography/types.py
@@ -0,0 +1,14 @@
+from typing import TypedDict
+
+class LocationData(TypedDict):
+    id: int
+    provinceCode: int
+    provinceNameEn: str
+    provinceNameTh: str
+    districtCode: int
+    districtNameEn: str
+    districtNameTh: str
+    subdistrictCode: int
+    subdistrictNameEn: str
+    subdistrictNameTh: str
+    postalCode: int
