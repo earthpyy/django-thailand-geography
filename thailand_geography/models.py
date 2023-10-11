3ddd05b2ddf5df30ea9082b2731b552b60babdaa
Preeti Yuankrathok <preetisatit@gmail.com>
2023-10-12 00:49:57 +0700

    Add index, add get province command

--- thailand_geography/models.py
+++ thailand_geography/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Province(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
-    name_en = models.CharField(max_length=100)
-    name_th = models.CharField(max_length=100)
+    name_en = models.CharField(max_length=100, db_index=True)
+    name_th = models.CharField(max_length=100, db_index=True)
 
     class Meta:
         ordering = ('id',)
@@ -13,8 +13,8 @@ class Province(models.Model):
 class District(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='districts')
-    name_en = models.CharField(max_length=100)
-    name_th = models.CharField(max_length=100)
+    name_en = models.CharField(max_length=100, db_index=True)
+    name_th = models.CharField(max_length=100, db_index=True)
 
     class Meta:
         ordering = ('id',)
@@ -23,8 +23,11 @@ class District(models.Model):
 class Subdistrict(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='subdistricts')
-    name_en = models.CharField(max_length=100)
-    name_th = models.CharField(max_length=100)
+    name_en = models.CharField(max_length=100, db_index=True)
+    name_th = models.CharField(max_length=100, db_index=True)
 
     class Meta:
         ordering = ('id',)
+
+    def get_province(self):
+        return self.district.province
