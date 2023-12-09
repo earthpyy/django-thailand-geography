caa43e04a51de34c58aee0bdb95eebf19c1de75d
Preeti Yuankrathok <preetisatit@gmail.com>
2023-12-09 18:23:23 +0700

    Fix lint

--- thailand_geography/admin.py
+++ thailand_geography/admin.py
@@ -16,7 +16,9 @@ class DistrictAdmin(admin.ModelAdmin):
 
 class SubdistrictAdmin(admin.ModelAdmin):
     list_display = ('id', 'province', 'district', 'name_en', 'name_th')
-    search_fields = ('name_en', 'name_th', 'province__name_en', 'province__name_th', 'district__name_en', 'district__name_th')
+    search_fields = (
+        'name_en', 'name_th', 'province__name_en', 'province__name_th', 'district__name_en', 'district__name_th',
+    )
     list_select_related = ('district__province',)
 
     def province(self, obj):
